b84046a6df9fbdd483d76da4a0a2d462ce47ef84
coopTRUE <coopdog4545@gmail.com>
2023-03-11 15:47:35 -0800

    initial files

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+  import 'normalize.css'
+  import '../global.scss'
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<main>
+  <h1>CRYPTO ARBITRAGE</h1>
+</main>
+
+<style lang="scss">
+  main {
+    display: grid;
+    place-items: center;
+    height: 100vh;
+  }
+  h1 {
+    font-size: 4rem;
+    font-weight: 500;
+    text-shadow: 0 0 10px #fff;
+  }
+</style>
